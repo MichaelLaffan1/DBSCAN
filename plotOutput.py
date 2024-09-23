49485413244991497159acffefcb15930875d1f8
frankdaseel <frankseelmann123@gmail.com>
2024-09-23 15:12:01 -0400

    update for space-delimiter instead of comma

--- plotOutput.py
+++ plotOutput.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("points.txt", header=None)
+df = pd.read_csv("dbscan/results.txt", header=None, delim_whitespace=True)
 df.columns = ['x', 'y', 'cluster_id']
 sns.set_style('darkgrid')
 sns.scatterplot(data=df, x='x', y='y', hue='cluster_id', palette='tab10', marker='o')
