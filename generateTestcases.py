45516a558d1270df388f0229d255f8cde610cfd8
frankdaseel <frankseelmann123@gmail.com>
2024-09-25 19:24:41 -0400

    try to make it scale with the grid dimensions

--- generateTestcases.py
+++ generateTestcases.py
@@ -1,21 +1,27 @@
 import random
 
-def generate_test_case(filename, epsilon, min_points, num_points):
+def generate_test_case(filename, epsilon_factor, min_points, num_points):
+    grid_size = num_points / 10
+    
+    epsilon = epsilon_factor * grid_size
+    
     with open(filename, 'w') as file:
         file.write(f"{epsilon} {min_points}\n")
         for i in range(num_points):
-            x = random.uniform(0, num_points/100)
-            y = random.uniform(0, num_points/100)
+            x = random.uniform(0, grid_size)
+            y = random.uniform(0, grid_size)
             file.write(f"{x} {y}\n")
 
 def main():
-    generate_test_case("testcase_1k.txt", 30, 5, 1000)
-    generate_test_case("testcase_2k.txt", 30, 5, 2000)
-    generate_test_case("testcase_5k.txt", 30, 5, 5000)
-    generate_test_case("testcase_10k.txt", 30, 5, 10000)
-    generate_test_case("testcase_20k.txt", 30, 5, 20000)
-    generate_test_case("testcase_50k.txt", 30, 5, 50000)
-    generate_test_case("testcase_100k.txt", 30, 5, 100000)
+    epsilon_factor = 0.1 
+    
+    generate_test_case("testcase_1k.txt", epsilon_factor, 5, 1000)
+    generate_test_case("testcase_2k.txt", epsilon_factor, 5, 2000)
+    generate_test_case("testcase_5k.txt", epsilon_factor, 5, 5000)
+    generate_test_case("testcase_10k.txt", epsilon_factor, 5, 10000)
+    generate_test_case("testcase_20k.txt", epsilon_factor, 5, 20000)
+    generate_test_case("testcase_50k.txt", epsilon_factor, 5, 50000)
+    generate_test_case("testcase_100k.txt", epsilon_factor, 5, 100000)
 
 if __name__ == "__main__":
     main()
